69966a1e45699eca4fc0b0eb0d8cc3205aa05b33
hanan <hananmalhajrii@gmail.com>
2023-06-02 00:48:48 +0300

    feat: Read files

--- minio/src/main/java/com/hanan/minio/MinioApplication.java
+++ minio/src/main/java/com/hanan/minio/MinioApplication.java
@@ -1,15 +1,25 @@
 package com.hanan.minio;
 
+import com.hanan.minio.components.MinIOComponent;
+import io.minio.errors.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class MinioApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ServerException, InvalidBucketNameException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
 
 		ApplicationContext applicationContext = SpringApplication.run(MinioApplication.class, args);
+		String filename = "test.txt";
+		MinIOComponent minIOComponent = applicationContext.getBean(MinIOComponent.class);
+		minIOComponent.readWriteMinIO(filename);
+
+		System.out.println("Done! ");
 	}
 
 }
